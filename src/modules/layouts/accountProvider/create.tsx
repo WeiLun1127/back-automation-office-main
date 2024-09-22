2ee313a3dca6ddff781c900db8da8224970f1eaf
LohWeiLun <137256771+LohWeiLun@users.noreply.github.com>
2024-09-22 12:53:46 +0800

    Corrected Account Provider Create Status

--- src/modules/layouts/accountProvider/create.tsx
+++ src/modules/layouts/accountProvider/create.tsx
@@ -1,10 +1,19 @@
 import SecurityIcon from "@mui/icons-material/Security";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import { Card, Checkbox, Grid, Icon, IconButton } from "@mui/material";
+import {
+  Autocomplete,
+  Card,
+  Checkbox,
+  Grid,
+  Icon,
+  IconButton,
+  Switch,
+  TextField,
+} from "@mui/material";
+import { apiHandler } from "api/apiHandler";
 import DashboardLayout from "assets/examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "assets/examples/Navbars/DashboardNavbar";
-import DataTable from "assets/examples/Tables/DataTable";
 import MDBox from "components/MDBox";
 import MDButton from "components/MDButton";
 import MDInput from "components/MDInput";
@@ -16,9 +25,38 @@ const CreateAccountProvider = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [prefix, setPrefix] = useState("");
+  const [isSwitchOn, setIsSwitchOn] = useState(false);
+  const [isTfaSwitchOn, setIsTfaSwitchOn] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
 
   const handlePasswordChange = (e: { target: { value: SetStateAction<string> } }) => {
     setPassword(e.target.value);
+  };
+
+  const controlOptions = [
+    { label: "Api Control", value: "apiControl" },
+    { label: "Master List", value: "masterList" },
+    { label: "Create Master Account", value: "createMasterAccount" },
+    { label: "Transactions", value: "transactions" },
+    { label: "Authentication", value: "authenthication" },
+    { label: "Create Account Provider", value: "createAccountProvider" },
+    { label: "Account Provider List", value: "accountProviderList" },
+    { label: "Currency List", value: "currencyList" },
+    { label: "Product List", value: "productList" },
+  ];
+
+  const controlMapping: { [key: string]: string[] } = {
+    apiControl: ["001", "001.001"],
+    masterList: ["002", "002.001"],
+    createMasterAccount: ["002", "002.002"],
+    transactions: ["003", "003.001"],
+    authenthication: ["004", "004.001"],
+    createAccountProvider: ["005", "005.001"],
+    accountProviderList: ["005", "005.002"],
+    currencyList: ["006", "006.001"],
+    productList: ["007", "007.001"],
   };
 
   const handleConfirmPasswordChange = (e: { target: { value: SetStateAction<string> } }) => {
@@ -36,7 +74,6 @@ const CreateAccountProvider = () => {
       setPasswordError("Passwords do not match.");
     } else {
       setPasswordError("");
-      // Proceed with form submission or further processing
     }
   };
 
@@ -46,6 +83,67 @@ const CreateAccountProvider = () => {
 
   const handleMouseDownPassword = () => {
     setShowPassword(true);
+  };
+
+  const handleSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsSwitchOn(event.target.checked); // Update switch state
+  };
+
+  const handleTfaSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsTfaSwitchOn(event.target.checked); // Update switch state
+  };
+
+  const storedUsername = localStorage.getItem("username");
+  const storedToken = localStorage.getItem("token");
+
+  const handleNextButtonClick = async () => {
+    if (password !== confirmPassword) {
+      setPasswordError("Passwords do not match.");
+      return;
+    }
+    try {
+      const apiUrl = "http://18.138.168.43:10311/api/execmem";
+      const selectedControlValues = selectedOptions.flatMap((option) => controlMapping[option]);
+      const controlArray = selectedControlValues.map((control) => ({
+        [control]: "1",
+      }));
+
+      const params = {
+        EXECF: "SETAUTHDATA",
+        Uid: storedUsername,
+        Token: storedToken,
+        Data: JSON.stringify({
+          Uid: userId,
+          Pass: password,
+          Control: JSON.stringify(controlArray),
+          Tfa: isTfaSwitchOn ? "1" : "0",
+          Tfakey: "1",
+          Class: "AP",
+          Prefix: prefix,
+          Status: isSwitchOn ? "1" : "0", // Convert boolean to string "1" or "0"
+        }),
+      };
+      const response = await apiHandler(apiUrl, params);
+      console.log("API Response:", response);
+
+      // Check if response.Status is 1
+      if (response.Status === "1") {
+        alert("Account Provider Created Successfully");
+
+        //Remove User Input
+        setUserId("");
+        setPassword("");
+        setConfirmPassword("");
+        setPrefix("");
+        setSelectedOptions([]);
+        setIsSwitchOn(false);
+        setIsTfaSwitchOn(false);
+      } else {
+        alert("Error Occured. Please Try Again Shortly");
+      }
+    } catch (error) {
+      console.error("Error during API call:", error);
+    }
   };
 
   return (
@@ -66,18 +164,22 @@ const CreateAccountProvider = () => {
                     fullWidth
                     variant="standard"
                     label="User ID"
+                    value={userId}
+                    onChange={(e: { target: { value: SetStateAction<string> } }) =>
+                      setUserId(e.target.value)
+                    }
                     InputProps={{ style: { maxWidth: "500px" } }}
                   />
                 </Grid>
 
-                <Grid item xs={12}>
+                {/* <Grid item xs={12}>
                   <MDInput
                     fullWidth
                     variant="standard"
                     label="Name"
                     InputProps={{ style: { maxWidth: "500px" } }}
                   />
-                </Grid>
+                </Grid> */}
 
                 <Grid item xs={12}>
                   <MDInput
@@ -132,20 +234,58 @@ const CreateAccountProvider = () => {
                     fullWidth
                     variant="standard"
                     label="Prefix"
+                    value={prefix} // Bind value to state
+                    onChange={(e: { target: { value: string } }) =>
+                      setPrefix(e.target.value.toUpperCase())
+                    } // Update state and convert to uppercase
                     InputProps={{ style: { maxWidth: "500px" } }}
                     inputProps={{
-                      maxLength: 3, // Restrict the input to 3 characters
-                      pattern: "[A-Za-z]{3}", // Regex pattern to allow only 3 alphabetic characters
+                      maxLength: 3,
+                      pattern: "[A-Za-z]{3}",
                     }}
-                    onChange={(e: { target: { value: any }; preventDefault: () => void }) => {
-                      const value = e.target.value.toUpperCase();
-                      // Allow only alphabetic characters and restrict the length to 3
-                      if (/^[A-Za-z]{0,3}$/.test(value)) {
-                        e.target.value = value;
-                      } else {
-                        e.preventDefault();
-                      }
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <MDInput
+                    fullWidth
+                    variant="standard"
+                    label="Level"
+                    value="AP"
+                    InputProps={{
+                      readOnly: true,
+                      style: { maxWidth: "500px" },
                     }}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Autocomplete
+                    multiple
+                    // options={controlOptions}
+                    options={controlOptions.filter(
+                      (option) => !selectedOptions.includes(option.value)
+                    )}
+                    disableCloseOnSelect
+                    getOptionLabel={(option) => option.label}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="standard"
+                        label="Control"
+                        style={{ maxWidth: "500px" }}
+                        fullWidth
+                      />
+                    )}
+                    value={selectedOptions.map((optionValue) =>
+                      controlOptions.find((option) => option.value === optionValue)
+                    )} // Map selected values to the corresponding options
+                    onChange={(event, value) =>
+                      setSelectedOptions(value.map((option) => option.value))
+                    }
+                    // onChange={(event, value) =>
+                    //   setSelectedOptions(value.map((option) => option.value))
+                    // }
                   />
                 </Grid>
 
@@ -153,7 +293,7 @@ const CreateAccountProvider = () => {
                   <MDBox display="flex" alignItems="center">
                     <MDTypography variant="button">Enable 2FA</MDTypography>
                     <SecurityIcon style={{ marginLeft: 8, marginRight: 8 }} />
-                    <Checkbox />
+                    <Checkbox checked={isTfaSwitchOn} onChange={handleTfaSwitchChange} />
                   </MDBox>
                 </Grid>
 
@@ -161,7 +301,10 @@ const CreateAccountProvider = () => {
                   <MDBox display="flex" alignItems="center">
                     <MDTypography variant="button">Show</MDTypography>
                     <Icon style={{ marginLeft: 8, marginRight: 8 }}>visibility</Icon>
-                    <Checkbox />
+                    <Switch
+                      checked={isSwitchOn} // Bind the checked state to isSwitchOn
+                      onChange={handleSwitchChange} // Call handleSwitchChange on toggle
+                    />
                   </MDBox>
                 </Grid>
               </Grid>
@@ -174,8 +317,13 @@ const CreateAccountProvider = () => {
                 mt={6}
               >
                 <MDBox display="flex" gap={2} justifyContent="flex-end" width="100%">
-                  <MDButton variant="gradient" color="dark" size="small">
-                    Next
+                  <MDButton
+                    variant="gradient"
+                    color="dark"
+                    size="small"
+                    onClick={handleNextButtonClick}
+                  >
+                    Create
                   </MDButton>
                 </MDBox>
               </MDBox>
